82cbb917277dd3a46d068cd0fab8035d87f535cc
henriquemarques <henriquedms7@gmail.com>
2024-08-13 18:26:35 +0200

    implement service method

---
+++ backend/src/main/java/org/example/backend/service/ExpenseService.java
@@ -0,0 +1,26 @@
+package org.example.backend.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.backend.DB.ExpenseRepo;
+import org.example.backend.model.Expense;
+import org.example.backend.model.ExpenseDto;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExpenseService {
+    private final ExpenseRepo expenseRepo;
+
+    public Expense addExpense(ExpenseDto userEntries) {
+        Expense newExpense = new Expense(
+                null,
+                userEntries.category(),
+                userEntries.vendor(),
+                userEntries.amount(),
+                userEntries.isCashPayment(),
+                userEntries.description(),
+                userEntries.date()
+        );
+        return expenseRepo.save(newExpense);
+    }
+}
