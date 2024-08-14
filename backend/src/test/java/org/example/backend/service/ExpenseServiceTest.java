8155f5567abb48b085268d69e4b4ebe3ce00a084
henriquemarques <henriquedms7@gmail.com>
2024-08-14 07:06:01 +0200

    add unit test for method

--- backend/src/test/java/org/example/backend/service/ExpenseServiceTest.java
+++ backend/src/test/java/org/example/backend/service/ExpenseServiceTest.java
@@ -6,6 +6,7 @@ import org.example.backend.model.ExpenseDto;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -14,6 +15,40 @@ class ExpenseServiceTest {
 
     private final ExpenseRepo expenseRepoMock = mock(ExpenseRepo.class);
     private final ExpenseService expenseService = new ExpenseService(expenseRepoMock);
+
+    @Test
+    void findAllExpenses() {
+        Expense expenseEx1 = new Expense(
+                null,
+                "food",
+                "edeka",
+                30.70,
+                false,
+                "",
+                LocalDate.of(2024,5,20)
+        );
+        Expense expenseEx2 = new Expense(
+                null,
+                "food",
+                "edeka",
+                30.70,
+                false,
+                "",
+                LocalDate.of(2024,5,20)
+        );
+
+        List<Expense> listOfExpensesEx = List.of(expenseEx1, expenseEx2);
+
+        when(expenseRepoMock.findAll()).thenReturn(listOfExpensesEx);
+
+        List<Expense> result = expenseService.findAllExpenses();
+
+        verify(expenseRepoMock).findAll();
+
+        assertEquals(listOfExpensesEx, result);
+
+
+    }
 
     @Test
     void addExpense() {
@@ -46,4 +81,5 @@ class ExpenseServiceTest {
         assertEquals(expense, result);
 
     }
+
 }
