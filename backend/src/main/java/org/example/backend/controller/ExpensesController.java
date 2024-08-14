79a83a5f9f4ed176978287c6c4411e47b0d93b9b
henriquemarques <henriquedms7@gmail.com>
2024-08-14 07:10:34 +0200

    add endpoint

--- backend/src/main/java/org/example/backend/controller/ExpensesController.java
+++ backend/src/main/java/org/example/backend/controller/ExpensesController.java
@@ -4,10 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.example.backend.model.Expense;
 import org.example.backend.model.ExpenseDto;
 import org.example.backend.service.ExpenseService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/expenses")
@@ -15,6 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExpensesController {
 
     private final ExpenseService expenseService;
+
+    @GetMapping
+    public List<Expense> getExpenses(){
+        return expenseService.findAllExpenses();
+    }
 
     @PostMapping
     public Expense postExpense(@RequestBody ExpenseDto userEntries){
