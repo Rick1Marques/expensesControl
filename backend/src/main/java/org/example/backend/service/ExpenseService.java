fdfeba276982657e4e0a274cf9cf0927b902ae91
henriquemarques <henriquedms7@gmail.com>
2024-08-14 07:06:17 +0200

    implement method

--- backend/src/main/java/org/example/backend/service/ExpenseService.java
+++ backend/src/main/java/org/example/backend/service/ExpenseService.java
@@ -6,6 +6,8 @@ import org.example.backend.model.Expense;
 import org.example.backend.model.ExpenseDto;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ExpenseService {
@@ -22,5 +24,9 @@ public class ExpenseService {
                 userEntries.date()
         );
         return expenseRepo.save(newExpense);
+    }
+
+    public List<Expense> findAllExpenses() {
+        return expenseRepo.findAll();
     }
 }
