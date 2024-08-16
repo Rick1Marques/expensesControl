3559b32202c7d7c057bf95595d1ce311af2d2868
henriquemarques <henriquedms7@gmail.com>
2024-08-16 16:11:32 +0200

    add Order sort clean code

--- frontend/src/components/ExpensesList.tsx
+++ frontend/src/components/ExpensesList.tsx
@@ -1,11 +1,16 @@
-import {useEffect, useState} from "react";
+import {ChangeEvent, useEffect, useState} from "react";
 import {Expense} from "../model/Expense.ts";
 import axios from "axios";
 import ExpenseCard from "./ExpenseCard.tsx";
 
 
+type Order = "asc" | "desc"
+type Field = "amount" | "date"
+
 export default function ExpensesList() {
     const [expenses, setExpenses] = useState<Expense[]>([])
+    const [sortOrder, setSortOrder] = useState<Order>("asc")
+    const [sortField, setSortField] = useState<Field>("date")
 
     useEffect(() => {
         async function fetchData() {
@@ -21,38 +26,62 @@ export default function ExpensesList() {
                 console.error("Error during fetch:", error)
             }
         }
+
         fetchData()
 
     }, [])
 
-    function listFilter(type: keyof Expense) {
-        const sortedExpenses = [...expenses].sort((a, b) => {
-            if(type === "amount"){
-                return b.amount - a.amount
-            } else {
+    useEffect(() => {
+        sortExpenses(sortField, sortOrder);
+    }, [expenses, sortOrder, sortField])
+
+
+    function sortExpenses(field: Field, order: Order) {
+        const sortedExpenses = expenses.sort((a, b) => {
+            let comparison = 0;
+            if (field === "amount") {
+                comparison = a.amount - b.amount
+            } else if (field === "date") {
                 const dateA = new Date(a.date).getTime()
                 const dateB = new Date(b.date).getTime()
-                return dateB - dateA;
+                comparison = dateA - dateB;
             }
-        });
-        setExpenses(sortedExpenses);
+            return order === "asc" ? comparison : -comparison
+        })
+
+        setExpenses(sortedExpenses)
     }
+
+
+    function handleFieldChange(field: Field) {
+        setSortField(field)
+    }
+
+    function handleOrderChange(event: ChangeEvent<HTMLSelectElement>) {
+        setSortOrder(event.target.value as Order)
+    }
+
 
     return (
         <section>
             <h2>Expenses List</h2>
-            <button onClick={()=>listFilter("date")}>Date</button>
-            <button onClick={()=>listFilter("amount")}>Amount</button>
+            <button onClick={() => handleFieldChange("date")}>Date</button>
+            <button onClick={() => handleFieldChange("amount")}>Amount</button>
+            <label htmlFor="sortOrder"/>
+            <select id="sortOrder" value={sortOrder} onChange={handleOrderChange}>
+                <option value="asc">Ascending</option>
+                <option value="desc">Descending</option>
+            </select>
             <ul>
-            {expenses.map(expense =>
-                <ExpenseCard
-                    key={expense.id}
-                    vendor={expense.vendor}
-                    amount={expense.amount}
-                    category={expense.category}
-                    date={expense.date}
-                    isCashPayment={expense.isCashPayment}
-                />)}
+                {expenses.map(expense =>
+                    <ExpenseCard
+                        key={expense.id}
+                        vendor={expense.vendor}
+                        amount={expense.amount}
+                        category={expense.category}
+                        date={expense.date}
+                        isCashPayment={expense.isCashPayment}
+                    />)}
             </ul>
         </section>
     )
