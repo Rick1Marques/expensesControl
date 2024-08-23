19da78bbdd5511eb95e721a073fa802d24e76511
henriquemarques <henriquedms7@gmail.com>
2024-08-23 17:11:51 +0200

    implement component BigChart

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,14 @@ import ExpensesContextProvider from "./store/expenses-context.tsx";
 import GroupList from "./components/GroupList.tsx";
 import PieChart from "./components/PieChart.tsx";
 import TimeRangeFilter from "./components/TimeRangeFilter.tsx";
+import BigChart from "./components/BigChart.tsx";
 
 export default function App() {
 
   return (
     <ExpensesContextProvider>
         <TimeRangeFilter/>
+        <BigChart/>
         <PieChart/>
         <GroupList title={"vendor"}/>
         <GroupList title={"category"}/>
--- frontend/src/components/PieChart.tsx
+++ frontend/src/components/PieChart.tsx
@@ -31,7 +31,6 @@ export default function PieChart() {
         ]
     }
 
-
     return (
         <>
             <Pie data={data}/>
