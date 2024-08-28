b793ee4f5c906dc312b1ff3e4014e89b090b9d79
henriquemarques <henriquedms7@gmail.com>
2024-08-28 14:00:49 +0200

    basic setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import ExpensesList from "./components/ExpensesList.tsx";
 import ExpensesContextProvider from "./store/expenses-context.tsx";
 import GroupList from "./components/GroupList.tsx";
@@ -6,18 +5,46 @@ import TimeRangeFilter from "./components/TimeRangeFilter.tsx";
 import BigChart from "./components/BigChart.tsx";
 import DetailsScreen from "./components/DetailsScreen.tsx";
 import FormDialog from "./components/FormDialog.tsx";
+import {Box, Container} from "@mui/material";
 
 export default function App() {
 
-  return (
-    <ExpensesContextProvider>
-        <TimeRangeFilter/>
-        <FormDialog type="add"/>
-        <DetailsScreen/>
-        <BigChart/>
-        <GroupList title={"vendor"}/>
-        <GroupList title={"category"}/>
-        <ExpensesList/>
-    </ExpensesContextProvider>
-  )
+    return (
+        <ExpensesContextProvider>
+            <Container sx={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "15px"
+            }}>
+                <Box
+                    sx={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                    }}>
+                    <TimeRangeFilter/>
+                    <FormDialog type="add"/>
+                </Box>
+
+                <Box
+                    sx={{
+                        display: "flex",
+                        justifyContent: "space-between"
+                    }}>
+                    <DetailsScreen/>
+                    <BigChart/>
+                </Box>
+
+                <Box
+                    sx={{
+                        display: "flex",
+                        justifyContent: "space-around"
+                    }}>
+                    <GroupList title={"vendor"}/>
+                    <GroupList title={"category"}/>
+                    <ExpensesList/>
+                </Box>
+
+            </Container>
+        </ExpensesContextProvider>
+    )
 }
--- frontend/src/components/TimeRangeFilter.tsx
+++ frontend/src/components/TimeRangeFilter.tsx
@@ -1,17 +1,18 @@
 import {useContext} from "react";
 import {ExpensesContext} from "../store/expenses-context.tsx";
+import {Box} from "@mui/material";
 
 export default function TimeRangeFilter() {
     const {handleChangeTimeRange, handleChangeRefDate, refDate} = useContext(ExpensesContext)
     return (
-        <section>
-            <label htmlFor="refDate">Ref date: </label>
-            <input type="date" id="refDate" value={refDate}
-                   onChange={(event) => handleChangeRefDate(event.target.value)}/>
+        <Box>
             <button onClick={() => handleChangeTimeRange("WEEK")}>Week</button>
             <button onClick={() => handleChangeTimeRange("MONTH")}>Month</button>
             <button onClick={() => handleChangeTimeRange("YEAR")}>Year</button>
             <button onClick={() => handleChangeTimeRange("ALL")}>All</button>
-        </section>
+            <label htmlFor="refDate">Ref date: </label>
+            <input type="date" id="refDate" value={refDate}
+                   onChange={(event) => handleChangeRefDate(event.target.value)}/>
+        </Box>
     )
 }
